8454cd8888a60b4177bdec69655bd7dc57ce987a
Mehmet Simsek <70211099+htmlhi@users.noreply.github.com>
2021-01-12 13:54:30 +0100

    Anmelde seite | input und background eingerichtet

---
+++ anmelden.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Willk​ommen">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Anmelden</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="anmelden.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.3.3, nicepage.com">
+
+
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    </header>
+    <head>
+
+    </head>
+    <body>
+        <h1>Anmelden</h1>
+        <form action="#" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" style="padding: 10px" source="custom" name="form">
+            <input type="hidden" id="siteId" name="siteId" value="7896959">
+            <input type="hidden" id="pageId" name="pageId" value="450626091">
+            <div class="u-form u-form-1">
+        <div class="u-form-group u-form-name">
+              <label for="name-de29" class="u-form-control-hidden u-label">Name</label>
+              <input type="text" placeholder="Name" id="name-de29" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+            </div>
+        <div class="u-form-group u-form-group-3">
+              <label for="text-1313" class="u-form-control-hidden u-label"></label>
+              <input type="password" placeholder="Passwort" id="text-1313" name="password" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white">
+            </div>
+        <div class="u-align-left u-form-group u-form-submit">
+              <a href="#" class="u-btn u-btn-submit u-button-style">absenden</a>
+                <p><input type="submit" value="submit" class="u-form-control-hidden"></p>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
