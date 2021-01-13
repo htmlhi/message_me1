2e85daaad83c7318faf4d617e76b5b5c7d26a1bb
Mehmet Simsek <70211099+htmlhi@users.noreply.github.com>
2021-01-13 09:44:46 +0100

    anmelden.php style geändert

--- anmelden.php
+++ anmelden.php
@@ -1,43 +1,57 @@
 <!DOCTYPE html>
-<html>
-<header>
+<html style="font-size: 16px;">
+  <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="keywords" content="Willk​ommen">
     <meta name="description" content="">
     <meta name="page_type" content="np-template-header-footer-from-plugin">
-    <title>Anmelden</title>
+    <title>Seite 1</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
 <link rel="stylesheet" href="anmelden.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 3.3.3, nicepage.com">
-
-
+    <meta name="generator" content="Nicepage 3.3.7, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
-    </header>
-    <head>
 
-    </head>
-    <body>
-        <h1>Anmelden</h1>
-        <form action="#" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" style="padding: 10px" source="custom" name="form">
-            <input type="hidden" id="siteId" name="siteId" value="7896959">
-            <input type="hidden" id="pageId" name="pageId" value="450626091">
-            <div class="u-form u-form-1">
-        <div class="u-form-group u-form-name">
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Seite 1">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body data-home-page="Seite-1.html" data-home-page-title="Seite 1" class="u-body"><header id="sec-2d70" data-image-width="1280" data-image-height="853"><div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-1">
+          <span class="u-text-body-alt-color" style="">Anmelden</span>
+        </h1>
+        <div class="u-form u-form-1">
+          <form action="#" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" style="padding: 10px" source="custom" name="form">
+            <div class="u-form-group u-form-name">
               <label for="name-de29" class="u-form-control-hidden u-label">Name</label>
               <input type="text" placeholder="Name" id="name-de29" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
             </div>
-        <div class="u-form-group u-form-group-3">
+            <div class="u-form-group u-form-group-2">
               <label for="text-1313" class="u-form-control-hidden u-label"></label>
-              <input type="password" placeholder="Passwort" id="text-1313" name="password" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white">
+              <input type="text" placeholder="Passwort" id="text-1313" name="password" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white">
             </div>
-        <div class="u-align-left u-form-group u-form-submit">
+            <div class="u-align-left u-form-group u-form-submit">
               <a href="#" class="u-btn u-btn-submit u-button-style">absenden</a>
-                <p><input type="submit" value="submit" class="u-form-control-hidden"></p>
-                </div>
+              <input type="submit" value="submit" class="u-form-control-hidden">
             </div>
-        </form>
-    </body>
+            <div class="u-form-send-message u-form-send-success"> Vielen Dank! Ihre Nachricht wurde gesendet. </div>
+            <div class="u-form-send-error u-form-send-message"> Ihre Nachricht konnte nicht gesendet werden. Bitte beheben Sie den Fehler und versuchen Sie es erneut. </div>
+            <input type="hidden" value="" name="recaptchaResponse">
+          </form>
+        </div>
+      </div></header>
+
+
+
+  </body>
 </html>
